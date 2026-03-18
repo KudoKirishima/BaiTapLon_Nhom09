ce1a927f3e7edd89e09c985a06415587cd251814
xhuy811-ctrl <x.huy811@gmail.com>
2026-03-18 09:21:40 +0700

    đăng nhập - hợp đồng

---
+++ Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLon_Nhom9
+{
+    public partial class frmQuanLy : Form
+    {
+        public frmQuanLy()
+        {
+            InitializeComponent();
+        }
+
+        private void btInhd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đã in hợp đồng" , "Thông báo" , 
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btThemhd_Click(object sender, EventArgs e)
+        {
+            frmThemHopDong f = new frmThemHopDong();
+            f.ShowDialog();
+        }
+    }
+}
